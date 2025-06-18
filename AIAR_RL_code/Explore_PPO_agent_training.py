956ba1002489ff9151963b18fba658429a40d4cf
leoniehoekstra <l.hoekstra@student.utwente.nl>
2025-06-18 17:13:40 +0200

    The script that I am using on the juptyer server

--- AIAR_RL_code/Explore_PPO_agent_training.py
+++ AIAR_RL_code/Explore_PPO_agent_training.py
@@ -1,17 +1,27 @@
 import os
 import shutil
+import socket
+import pathlib
+from pathlib import Path
 import ray
+from ray.air import RunConfig
 
 from ray.tune.registry import register_env
 from explore_agent.envs.exploring_gym import ExploreDrone
 from ray.rllib.algorithms.ppo.ppo import PPOConfig
 from ray._private import resource_spec
-# Monkey patch to skip nvidia-smi check
+
 def _autodetect_num_gpus():
-    return 0
+    return 1
 
 resource_spec._autodetect_num_gpus = _autodetect_num_gpus
-ray.init(ignore_reinit_error=True, include_dashboard=False, _temp_dir=r"..\tmp\ray")
+
+ray.init(
+    _node_ip_address="127.0.0.1", # otherwise it crashes on the university serverrr
+    include_dashboard=False,
+    ignore_reinit_error=True,
+    _temp_dir=str(Path.home() / "tmp" / "ray"),
+)
 
 
 chkpt_root = "tmp/ppo/checkpoint_{}"
@@ -20,16 +30,38 @@ ray_results = "{}/ray_results/".format(os.getenv("HOME"))
 
 select_env = "ExploreAgent-v0"
 register_env(select_env, lambda config: ExploreDrone({"env_name": "playground",  "reward_mode": "continuous"}))
-config = PPOConfig().resources(num_gpus=1).rollouts(num_rollout_workers=24).framework("torch")
 
+from ray.rllib.algorithms.ppo import PPOConfig
 
+config = (
+    PPOConfig()
+      .environment(select_env)
+      .framework("torch")
+      .resources(
+          num_gpus=1,
+          num_cpus_per_worker=1,
+          num_gpus_per_worker=0
+      )
+      .rollouts(num_rollout_workers=70)
+      .training(
+          lr=1e-4,
+          train_batch_size=280_000,
+          sgd_minibatch_size=32_768,
+          num_sgd_iter=10,
+          entropy_coeff=0.01,
+          entropy_coeff_schedule=[[0, 0.01], [2e6, 0.0]],
+          clip_param=0.2,
+          kl_coeff=0.2,
+          kl_target=0.01,
+      )
+)
 
-agent = config.build(env=select_env)
+agent = config.build()
 status = "{:2d} reward {:6.2f}/{:6.2f}/{:6.2f} len {:4.2f}"
 best_reward = float("-inf")
 best_checkpoint = None
 
-n_iter = 500
+n_iter = 300
 save_interval = 10
 warmpup_iterations = 50
 
